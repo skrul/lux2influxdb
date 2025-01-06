f8a9614dd096963fe040eaa55fc04d97d281acb6
root <root@docker.local>
2025-01-06 13:38:21 -0800

    add voltages

--- metrics.py
+++ metrics.py
@@ -1,94 +1,110 @@
 METRICS = {
     "Pload": {
         "type": "gauge",
-        "unit": "W",
     },
     "Pall": {
         "type": "gauge",
-        "unit": "W",
     },
     "Ppv1": {
         "type": "gauge",
-        "unit": "W",
     },
     "Ppv2": {
         "type": "gauge",
-        "unit": "W",
     },
     "Ppv3": {
         "type": "gauge",
-        "unit": "W",
     },
     "Ptouser": {
         "type": "gauge",
-        "unit": "W",
     },
     "Ptogrid": {
         "type": "gauge",
-        "unit": "W",
     },
     "Pcharge": {
         "type": "gauge",
-        "unit": "W",
     },
     "Pdischarge": {
         "type": "gauge",
-        "unit": "W",
     },
     "SOC": {
         "type": "gauge",
-        "unit": None,
     },
     "Epv1_day": {
         "type": "gauge",
-        "unit": None,
     },
     "Epv2_day": {
         "type": "gauge",
-        "unit": None,
     },
     "Epv3_day": {
         "type": "gauge",
-        "unit": None,
     },
     "Epv_all": {
         "type": "gauge",
-        "unit": None,
     },
     "Einv_day": {
         "type": "gauge",
-        "unit": None,
     },
     "Erec_day": {
         "type": "gauge",
-        "unit": None,
     },
     "Echg_day": {
         "type": "gauge",
-        "unit": None,
     },
     "Edischg_day": {
         "type": "gauge",
-        "unit": None,
     },
     "Eeps_day": {
         "type": "gauge",
-        "unit": None,
     },
     "Etogrid_day": {
         "type": "gauge",
-        "unit": None,
     },
     "Etouser_day": {
         "type": "gauge",
-        "unit": None,
     },
     "Edischg_day": {
         "type": "gauge",
-        "unit": None,
     },
     "Edischg_day": {
         "type": "gauge",
-        "unit": None,
+    },
+    "Vpv1": {
+        "type": "gauge",
+    },
+    "Vpv2": {
+        "type": "gauge",
+    },
+    "Vpv3": {
+        "type": "gauge",
+    },
+    "Vbus1": {
+        "type": "gauge",
+    },
+    "Vbus2": {
+        "type": "gauge",
+    },
+    "VbusP": {
+        "type": "gauge",
+    },
+    "Vbat": {
+        "type": "gauge",
+    },
+    "VacR": {
+        "type": "gauge",
+    },
+    "VacS": {
+        "type": "gauge",
+    },
+    "VacT": {
+        "type": "gauge",
+    },
+    "VepsR": {
+        "type": "gauge",
+    },
+    "VepsS": {
+        "type": "gauge",
+    },
+    "VepsT": {
+        "type": "gauge",
     },
 }
